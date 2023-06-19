4eb7dc140cb01f745e4c86924684888912cbd36e
larawb <135658858+larawb@users.noreply.github.com>
2023-06-19 13:20:23 -0300

    Update funciones.py
    Agregado de funciones restantes al final del 1er semestre.

--- funciones.py
+++ funciones.py
@@ -41,3 +41,241 @@ def leer_google_spreadsheet(doc_id : str, sheet_name : str = None) -> list :
     _spreadsheets_recordados[(doc_id, sheet_name)] = (workbook.title, sheet.title, result)
     print('Se leyó la hoja', sheet.title, 'de', workbook.title)
     return result
+############################################################
+def leer_requerimientos() -> dict:
+    """ Lectura de la planilla de Requerimientos de inscripción.
+
+    Resultado
+    -------
+    dict: pasaporte -> Solicitud
+        Si bien los datos se leen de la planilla de Requerimientos, la estructura se toma de las Solicitudes.
+        
+        Solicitud es dict {
+                'Correo' : str, 
+                'Nombre' : str, 
+                'Elecciones' : [ Eleccion ]
+            }
+        Eleccion es dict, con claves
+            [ 'Escuela', 'Codigo', 'Materia', 'Horario', 'Prioridad' ]
+        siempre asociadas a str.
+    """
+    # # from https://stackoverflow.com/a/423596
+    # # You can use a global variable within functions by declaring it as `global`
+    # global requerimientos
+
+    # # from https://stackoverflow.com/a/843293
+    # # To check the existence of a local variable:
+    # # if 'myVar' in locals(): ...
+    # # To check the existence of a global variable:
+    # # if 'myVar' in globals(): ...
+
+    requerimientos = leer_google_spreadsheet(id_requerimientos_de_inscripcion_manual)
+
+    index = { }
+    for i, column in enumerate(requerimientos[0]) : # encabezado
+        index[column] = i
+    #print(index) # {'Correo': 0, 'Nombre': 1, 'Pasaporte': 2, 'Escuela': 3, 'Codigo': 4, 'Materia': 5, 'Horario': 6, 'Prioridad': 7}
+
+    result = { }
+    for row in requerimientos[1:] : # saltea encabezado
+        pasaporte = row[index['Pasaporte']]
+        if not pasaporte in result :
+            result[pasaporte] = {
+                    'Correo' : row[index['Correo']], 
+                    'Nombre' : row[index['Nombre']], 
+                    'Elecciones' : [ ]
+                }
+        eleccion = { }
+        atributos = ['Escuela', 'Codigo', 'Materia', 'Horario', 'Prioridad']
+        for it in atributos :
+            eleccion[it] = row[index[it]]
+        
+        result[pasaporte]['Elecciones'].append(eleccion)
+
+    key = list(result.keys())[0] # primer elemento
+    print('Ejemplo: requerimientos[' + key + ']:', result[key])
+    return result
+############################################################
+def leer_ordenes_SIU3() -> dict :
+    """ Leer planilla SIU3 de las Órdenes de inscripción.
+    
+    Resultado
+    ---------
+    dict :
+        clave pasaporte
+        valor [ orden ]
+    """
+    ordenes_SIU3 = leer_google_spreadsheet(id_2023C1_ordenes_de_inscripcion, 'SIU3')
+    result = { }
+    encabezado = ordenes_SIU3[0]
+    atributos_eleccion = ['Escuela', 'Codigo', 'Materia', 'Horario', 'Prioridad']
+    for row in ordenes_SIU3[1:] : # saltea encabezado
+        orden = { }
+        #print('row', row)
+        for attr in atributos_eleccion :
+            orden[attr] = row[encabezado.index(attr)]
+
+        # TODO: decidir a qué se llama 'orden' y a qué 'eleccion'. Quizás
+        # convenga unificar los nombres.
+
+        #print('orden:', orden)
+        # ('Escuela', 'Escuela de Humanidades')
+        # ('Codigo', 'MA-FC00254')
+        # ('Materia', 'Filosofía de la cultura')
+        # ('Horario', 'lunes noche')
+        # ('Prioridad', 'principal')
+
+        # TODO: definir cómo se llama y cómo se asocia al resto:
+        # ('Estado', 'Inscripto')
+        # ('Observaciones', '')
+        # Problemas:
+        # - No se están leyendo los 'Estados' de las Ordenes SIU2
+        # - SIU2 no tiene del todo definidos los Estados
+        # PROVISORIO...
+        # Se agrega
+        estado = row[encabezado.index('Estado')]
+        orden['Estado'] = estado
+
+        pasaporte = row[encabezado.index('Pasaporte')]
+        # OJO que 'pasaporte' puede ser banana si...
+        #   orden: {'Escuela': '', 'Codigo': '', 'Materia': 'a', 'Horario': '', 'Prioridad': ''}
+        # NOTAR la 'a' en Materia
+
+        if estado == '' :
+            print("¡CUIDADO! En la planilla SIU3, el Estado == '' se asume " +
+                  "'Inscripto' para el pasaporte", pasaporte, "en la orden", orden)
+
+        if not pasaporte in result :
+            result[pasaporte] = [ ]
+        result[pasaporte].append(orden)
+    return result
+############################################################
+def leer_ordenes_SIU2(sheet_name : str) -> dict :
+    ordenes_SIU2 = leer_google_spreadsheet(id_2023C1_ordenes_de_inscripcion, sheet_name)
+    # atributos_eleccion = ['Escuela', 'Codigo', 'Materia', 'Horario', 'Prioridad'] # SIU3
+    # atributos_eleccion = ['Materia', 'Horario', 'Prioridad'] # SIU2 original
+    atributos_eleccion = ['Materia', 'Horario', 'Prioridad', 'Estado'] # SIU2 emparchado
+    result = convertir_en_dict(ordenes_SIU2, 'Pasaporte', atributos_eleccion)
+    return result
+############################################################
+def convertir_en_dict(rows : list, key_name : str, cols : list) -> dict :
+    result = { }
+    encabezado = rows[0]
+    for row in rows[1:] : # saltea encabezado
+        value = { }
+        #print('row', row)
+        for col in cols :
+            value[col] = row[encabezado.index(col)]
+
+        key = row[encabezado.index(key_name)]
+        # OJO que key o value pueden ser banana en filas con celdas vacías.
+
+        if not key in result :
+            result[key] = [ ]
+        result[key].append(value)
+    return result
+############################################################
+_encabezado_incoming_recordado = None
+def leer_encabezado_incoming() -> list :
+    global _encabezado_incoming_recordado
+    if _encabezado_incoming_recordado == None :
+        # al leer incoming se define el encabezado recordado
+        leer_listado_incoming()
+    return _encabezado_incoming_recordado
+
+_listado_incoming_recordado = None
+def leer_listado_incoming(reportar_sin_pasaporte : bool = True) -> dict :
+    """ Lectura de `incoming/listado.php`.
+
+    Resultado
+    ---------
+    dict: pasaporte -> Persona
+
+        Persona es dict, con claves [
+            'Fecha', 'Apellido', 'Nombre', 'Nacionalidad', 'Carrera', 
+            'Universidad', 'Pais', 'Email', 'Email Alternativo', 'Periodo', 
+            'Anio', 'Sexo', 'Pasaporte', 'Fecha Nacimiento', 'E-Mail Responsable'
+        ]
+        siempre asociadas a str, aunque no siempre están todas presentes.
+
+    Pendiente
+    ---------
+        * Definir cómo procesar a las personas sin pasaporte.
+    """
+
+    # from https://stackoverflow.com/a/423596
+    # You can use a global variable within functions by declaring it as `global`
+    global _listado_incoming_recordado
+    if _listado_incoming_recordado != None :
+        print('Se recordó incoming/listado.php.')
+        return _listado_incoming_recordado
+
+    !pip install xmltodict
+
+    from xml.dom import minidom
+    import requests
+    import xmltodict
+
+    # en listado.php falta:
+    #   - <! DOCTYPE html>
+    #   - La codificación de caracteres del documento no fue declarada
+    url = "http://www.unsam.edu.ar/internacional/incoming/listado.php"
+    response = requests.get(url) # TARDA
+    #print('encoding', response.apparent_encoding) # encoding Windows-1252
+
+    # FALLAN MISERABLEMENTE
+    #dom = minidom.parseString(str)
+    #data = xmltodict.parse(response.content, encoding='Windows-1252')
+
+    # Correcciones necesarias para poder parsear
+    str = response.content.decode('Windows-1252')
+    str = str.replace("border=1", "border='1'") # entrecomillado del atributo
+    str = str.replace("&", "&")             # encoding del '&'.
+    #print(str)
+    # dom = minidom.parseString(str)
+    # print(dom)
+    # #dom.getElementsByTagName("tr")[0]
+    # print(dom.getElementsByTagName("tr")) # tr: table-row
+    # for elem in dom.getElementsByTagName("tr") : # tr: table-row # FUNCIONA
+    #     elem.getElementsByTagName...
+
+    dict = xmltodict.parse(str)
+    # print('dict-224', dict) # {'table': {'@border': '1', 'tr': [{'td': 
+    #       ['Fecha', 'Apellido', 'Nombre', 'Nacionalidad', 'Carrera', 'Universidad', 'Pais', 'Email', 'Email Alternativo', 'Periodo', 'Anio', 'Sexo', 'Pasaporte', 'Fecha Nacimiento', 'E-Mail Responsable']}, 
+    #   {'td': [None, 'López', 'Cecilia', 'Argentina', 'Psicopedagogía', 'Free Mover', 'Argentina', 'snielson@unsam.edu.ar', 'snielson@unsam.edu.ar', None, None, None, None, None, None]}, , ...}]}}
+    dict = dict['table']['tr'] # lista de {'td': [valores]}
+    encabezado = dict.pop(0)['td'] # ['Fecha', 'Apellido', 'Nombre', 'Nacionalidad', 'Carrera', 'Universidad', 'Pais', 'Email', 'Email Alternativo', 'Periodo', 'Anio', 'Sexo', 'Pasaporte', 'Fecha Nacimiento', 'E-Mail Responsable']
+    # index_pasaporte = encabezado.index('Pasaporte')
+    global _encabezado_incoming_recordado
+    _encabezado_incoming_recordado = encabezado
+    # print('encabezado', encabezado)
+    #print(dict)
+    #  {'td': [None, 'López', 'Cecilia', 'Argentina', 'Psicopedagogía', 'Free Mover', 'Argentina', 'snielson@unsam.edu.ar', 'snielson@unsam.edu.ar', None, None, None, None, None, None]}, {'td': [None, 'Rodríguez Sánchez', 'Adrián Gerardo', 'Mexicana', 'Posgrado en Historiografía', 'Free Mover', 'Argentina', 'adrian.geros@gmail.com', 'adrian.geros@gmail.com', None, None, None, None, None, None]}, ...]}]
+    #index_pasaporte = encabezado.index('Pasaporte')
+
+    # Diccionario Pasaporte -> Persona
+    result = { }
+
+    for row in dict :
+        #values = row['td']     #[None, 'López', 'Cecilia', 'Argentina', 'Psicopedagogía', 'Free Mover', 'Argentina', 'snielson@unsam.edu.ar', 'snielson@unsam.edu.ar', None, None, None, None, None, None]
+        #pasaporte = tr['td'][index_pasaporte]
+        persona = { }
+        for index, value in enumerate(row['td']) :
+            if value != None : # saltea valores de celdas vacías
+                persona[encabezado[index]] = value
+            # si no hay pasaporte, ¿usamos 'Email'?
+            #if index == index_pasaporte and value == None and reportar_sin_pasaporte:
+            #    print("Esta persona no tiene definido el pasaporte: ", persona)
+        if 'Pasaporte' in persona :
+            pasaporte = persona.pop('Pasaporte')
+            result[pasaporte] = persona
+        elif reportar_sin_pasaporte :
+            print("Esta persona no tiene definido el pasaporte: ", persona)
+
+    _listado_incoming_recordado = result    
+    print('Leído:', url)
+    # print("La variable 'incoming' es un diccionario Pasaporte -> DatosPersonales.")
+    # print('Las claves del diccionario DatosPersonales son: ', encabezado)
+    # print('Por ejemplo: "incoming[1234] == { Apellido : Messi, Nombre : Lionel, Pais : Argentina }".')
+
+    return result
